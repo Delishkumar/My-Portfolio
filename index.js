760b937418db5d254b1032ebe6298cc219240bed
Delishkumar <delishkumar800@gmail.com>
2025-03-24 11:15:07 +0530

    update skil

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-document.querySelectorAll("nav ul li a").forEach(link => {
-    link.addEventListener("click",function(){
-        document.querySelectorAll("nav ul li a").forEach(a =>{
-            a.classList.remove("active")
-           this.classList.add("active")
-        })
-    })
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    navLinks.forEach(nav => nav.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+
+ // slidemenu//
+ document.getElementById("cut").addEventListener("click",function(){
+  document.querySelector(".slidelists").style.display="none"
+})
+
+document.getElementById("menubutn").addEventListener("click",function(){
+  document.querySelector(".slidelists").style.transform= "translateX(-10px)";
 })
